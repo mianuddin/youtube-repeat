f5fdff20b32d9b03e9d4c10aed1f76bde7180f2f
Mian Uddin <mianuddin@gmail.com>
2015-01-11 04:49:43 +0000

    Added support for YouTube urls.

--- js/script.js
+++ js/script.js
@@ -12,11 +12,24 @@ function GetURLParameter(sParam)
     }
 }
 
+function parseInput(input) {
+    if(typeof input === 'undefined') {
+        return undefined;
+    } else {
+        var decodedInput = decodeURIComponent(input);
+        console.log(decodedInput);
+        if(decodedInput.indexOf('?v=') != -1) {
+            return decodedInput.slice(decodedInput.indexOf('?v=')+3);
+        }
+        return decodedInput;
+    }
+} 
+
 $( document ).ready(function() {
-    var id = GetURLParameter('v');
+    var id = parseInput(GetURLParameter('v'));
     var link = '//www.youtube.com/embed/' + id + '?rel=0&amp;controls=0&amp;showinfo=0&autoplay=1&loop=1&playlist=' + id;
     if(typeof id != 'undefined') {
-        $('input[type=text]').attr('value', id);
+        $('input[type=text]').attr('value', decodeURIComponent(GetURLParameter('v')));
         $('iframe').attr('src', link);
     } else {
         $('#videoWrapper').hide();
